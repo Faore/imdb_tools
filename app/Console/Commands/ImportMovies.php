6f89b8ed622e6cdda1525310d9da0b3800d01506
Miriam <miriam.rognlie@gmail.com>
2017-10-12 21:36:59 -0600

    Movie Import

---
+++ app/Console/Commands/ImportMovies.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ImportMovies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:movie';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports movies into the database.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $file_handle = fopen("data/IMDB-AWS/Movie,Show,Episode/data.tsv", "r");
+        $row = 0;
+        while (!feof($file_handle)) {
+            $line = trim(fgets($file_handle));
+            $array = explode("\t", $line);
+            if($row != 0) {
+                if($array[1]=='Movie'||$array[1]=='Short'){
+                    $Type = $array[1];
+                    $Title = $array[2];
+                    $Year = $array[5];
+                    $Runtime = $array[7];
+                    $AdultContent = $array[4];
+                    if($Year == '\N') {
+                        $Year = null;
+                    }
+                    DB::table('Movie')->insert([
+                        'Title' => $Title,
+                        'Year' => $Year,
+                        'Runtime' => $Runtime,
+                        'Type' => $Type,
+                        'AdultContent' => $AdultContent,
+                    ]);
+                }
+                if($row % 100 == 0) {
+                    $this->info("Inserted $row rows.");
+                }
+            }
+
+            $row++;
+        }
+        fclose($file_handle);
+    }
+}
