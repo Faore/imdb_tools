e93456e59e81eefb9ff615a88dc0f5bb3ee6afb2
Christopher E. Thomas <chris@admt.im>
2017-10-14 20:33:57 -0600

    things

---
+++ app/Helper/DataReader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cthomas
+ * Date: 10/14/2017
+ * Time: 8:32 PM
+ */
+
+namespace App\Helper;
+
+
+class DataReader
+{
+    
+}
---
+++ config/data.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    '5000' => [
+        'file' => 'data/IMDB-5000/movie_metadata.csv',
+        'delimiter' => ','
+    ],
+    '1000' => [
+        'file' => 'data/IMDB-1000/IMDB-Movie-Data.csv',
+        'delimiter' => ','
+    ],
+    'AWS' => [
+        'name_basics' => [
+            'file' => 'data/IMDB-AWS/name.basics.tsv/data.tsv',
+            'delimiter' => "\t"
+        ],
+
+        'title_basics' => [
+            'file' => 'data/IMDB-AWS/title.basics.tsv/data.tsv',
+            'delimiter' => "\t"
+        ]
+        ,
+
+        'title_crew' => [
+            'file' => 'data/IMDB-AWS/title.crew.tsv/data.tsv',
+            'delimiter' => "\t"
+        ]
+        ,
+
+        'title_episode' => [
+            'file' => 'data/IMDB-AWS/title.episode.tsv/data.tsv',
+            'delimiter' => "\t"
+        ]
+        ,
+
+        'title_principals' => [
+            'file' => 'data/IMDB-AWS/title.principals.tsv/data.tsv',
+            'delimiter' => "\t"
+        ]
+        ,
+
+        'title_ratings' => [
+            'file' => 'data/IMDB-AWS/title.ratings.tsv/data.tsv',
+            'delimiter' => "\t"
+        ]
+
+    ]
+];
