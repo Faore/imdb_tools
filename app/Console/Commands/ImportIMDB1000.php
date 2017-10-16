8cb4132481cab044dc0522a35f5c28e8aa9499c2
Miriam <miriam.rognlie@gmail.com>
2017-10-15 23:40:33 -0600

    update from 1000

--- app/Console/Commands/ImportIMDB1000.php
+++ app/Console/Commands/ImportIMDB1000.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use \App\Helper\DataReader;
+use Illuminate\Support\Facades\DB;
 
 class ImportIMDB1000 extends Command
 {
@@ -11,14 +13,14 @@ class ImportIMDB1000 extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'import:1000';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Update movies with info from 1000';
 
     /**
      * Create a new command instance.
@@ -38,5 +40,23 @@ class ImportIMDB1000 extends Command
     public function handle()
     {
         //1000 contains 0:rank 1:Title(R) 2:Genre(R) 3:Description 4:Director(R) 5:Actors 6:Year(R) 7:Runtime (Minutes)(R) 8:Rating 9:Votes(R) 10:Revenue (Millions) 11:Metascore
+        $config = \Config::get('data.1000');
+        $file = Datareader::openDataFile($config);
+        while (!DataReader::endOfFile($file)) {
+            $row = DataReader::getNextRow($file);
+            $movieId = 0;
+            $movieId = DB::table('Movie')->where('Title', '=', $row['Title'])->where('Year', '=', $row['Year'])->where('Rank', '=', null)->first()->id;//find corresponding movie added from 5000 which has not been updated with 1000 data
+            if($movieId!=null){
+                $this->info("updating movie with id $movieId");
+                DB::table('Movie')->where('id', '=', $movieId)->update([
+                    'Rank' => $row['Rank'],
+                    'Rating'=> $row['Rating'],
+                    'Votes'=> $row['Votes'],
+                    'Metascore'=>$row['Metascore'],
+                    'Revenue'=>$row['Revenue (Millions)'],
+                ]);
+                break;
+            }
+        }
     }
 }
